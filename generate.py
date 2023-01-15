6383f15e0ec834f55b7062d604d2b932a5c53242
KaioKumagai <kaiokumagai2025@u.northwestern.edu>
2023-01-15 14:41:09 -0600

    Beginning of sensors

---
+++ generate.py
@@ -0,0 +1,50 @@
+import pyrosim.pyrosim as pyrosim
+
+length = 1
+width = 1
+height = 1
+
+x = 0
+y = 0
+z = 1.0
+
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+
+    x = 3
+    y = 3
+    
+    pyrosim.Send_Cube(name="Box", pos=[3,3,1] , size=[length,width,height]) 
+
+    pyrosim.End()
+
+
+x = 0
+y = 0
+z = 1.0
+
+Link0 = [0.5,0,0.5]
+Link1 = [0.5,0,0.5]
+Link2 = [0.5,0,-0.5]
+# Link2 = [0.5,0,0]
+Link0_Link1 = [1,0,1]
+Link1_Link2 = [1,0,0]
+# Link1_Link2 = [1.5,0,0.5]
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    # pyrosim.Send_Cube(name="Torso", pos=[0,0,1] , size=[length,width,height]) 
+    # pyrosim.Send_Joint( name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = [1,0,1.5])
+    # pyrosim.Send_Cube(name="Leg", pos=[1,0,1.5] , size=[length,width,height]) 
+    pyrosim.Send_Cube(name="Torso", pos=Link0 , size=[length,width,height]) 
+    pyrosim.Send_Joint( name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = Link0_Link1)
+    pyrosim.Send_Cube(name="Leg", pos=Link1 , size=[length,width,height]) 
+    pyrosim.Send_Joint( name = "Leg_Arm" , parent= "Leg" , child = "Arm" , type = "revolute", position = Link1_Link2)
+    pyrosim.Send_Cube(name="Arm", pos=Link2 , size=[length,width,height]) 
+    pyrosim.End()   
+
+Create_World()
+
+Create_Robot()
+
+
--- simulate.py
+++ simulate.py
@@ -9,11 +9,12 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 
 planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
 
 p.loadSDF("world.sdf")
 
 
-for t in range(1000):
+for t in range(2000):
     p.stepSimulation()
     time.sleep(1/60)
     print (t)
